a0f96de26f67b426df4d6f646a416892ba9be18a
ch9eri <limch9eri@gmail.com>
2022-09-20 19:57:08 +0900

    [FEAT] input값 todoList에 추가하기

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from './components/Input';
 import TodoList from './components/TodoList';
 import DoneList from './components/DoneList';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
   return (
     <div>
-      <Input>
-        <TodoList />
-        <DoneList />
-      </Input>
+      <Input todoList={todoList} setTodoList={setTodoList} />
+      <TodoList />
+      <DoneList />
     </div>
   );
 }
--- todo/src/components/Input.jsx
+++ todo/src/components/Input.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Input = () => {
+const Input = ({ todoList, setTodoList }) => {
+  const [text, setText] = useState('');
+
+  //input 값 가져오기
+  const handleInput = (e) => {
+    setText(e.target.value);
+  };
+
+  const handlePlusButton = () => {
+    //todoList에 input 값 추가
+    const newTodoList = todoList.concat({
+      id: Date.now(),
+      text,
+    });
+    setTodoList(newTodoList);
+    setText('');
+  };
+
   return (
     <div className="todo-input">
       <input
         className="input-box"
         type="text"
+        value={text}
         placeholder="할 일을 입력하세요"
+        onChange={handleInput}
       />
-      <button className="plus-btn" type="submit">
+      <button className="plus-btn" type="submit" onClick={handlePlusButton}>
         ➕
       </button>
     </div>
