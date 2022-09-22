0d03385ea85823fb57387fa6764c7a9348bb5fd3
ch9eri <limch9eri@gmail.com>
2022-09-22 21:23:52 +0900

    [FEAT] 랜덤 명언 출력 기능

--- todo/src/App.js
+++ todo/src/App.js
@@ -6,6 +6,20 @@ import { createGlobalStyle } from 'styled-components';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const proverbs = [
+    '한줄 코드라도 서로 거들면 낫다',
+    '발없는 오픈소스 천리간다',
+    '코딩하다 곧 중지하면 아니한만 못하니라',
+    '보기 좋은 코드가 디버깅 하기도 좋다',
+    '늦게 배운 코딩 날새는줄 모른다',
+    '완벽한 코드도 생각해보고 컴파일하자',
+    '코딩 전 마음 다르고, 코딩 후 마음 다르다',
+  ];
+
+  function randomProverbs(array) {
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
+  }
 
   return (
     <>
@@ -16,6 +30,7 @@ function App() {
           <Input todoList={todoList} setTodoList={setTodoList} />
           <TodoList todoList={todoList} setTodoList={setTodoList} />
         </Container>
+        <Proverbs>"{randomProverbs(proverbs)}"</Proverbs>
       </Wrapper>
     </>
   );
@@ -42,6 +57,7 @@ const Wrapper = styled.div`
   align-items: center;
   height: 100vh;
   background-color: pink;
+  flex-direction: column;
 `;
 
 const Container = styled.div`
@@ -56,6 +72,11 @@ const Container = styled.div`
 const Title = styled.h1`
   text-align: center;
   color: pink;
+`;
+
+const Proverbs = styled.div`
+  margin-top: 20px;
+  font-weight: bold;
 `;
 
 export default App;
